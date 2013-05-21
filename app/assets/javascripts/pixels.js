e06aba2d1fded9afc70e95d42fd230d1cda686ef
hayksaakian <fakest.id.ever@gmail.com>
2013-05-21 13:20:25 -0700

    fix table sorting bugs

--- app/assets/javascripts/pixels.js
+++ app/assets/javascripts/pixels.js
@@ -6,42 +6,45 @@ var heatmap;
 var mapData;
 
 window.onload = function(){
-  var myLatlng = new google.maps.LatLng(40.4230, -98.7372);
+  if($('#heatmapArea').length > 0){
+    var myLatlng = new google.maps.LatLng(40.4230, -98.7372);
 
-  var myOptions = {
-    zoom: 3,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    disableDefaultUI: false,
-    scrollwheel: true,
-    draggable: true,
-    navigationControl: true,
-    mapTypeControl: false,
-    scaleControl: true,
-    disableDoubleClickZoom: false
-  };
+    var myOptions = {
+      zoom: 3,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: false,
+      scrollwheel: true,
+      draggable: true,
+      navigationControl: true,
+      mapTypeControl: false,
+      scaleControl: true,
+      disableDoubleClickZoom: false
+    };
 
-	map = new google.maps.Map(document.getElementById("heatmapArea"), myOptions);
-  heatmap = new HeatmapOverlay(map, {"radius":10, "visible":true, "opacity":60});
+  	map = new google.maps.Map(document.getElementById("heatmapArea"), myOptions);
+    heatmap = new HeatmapOverlay(map, {"radius":10, "visible":true, "opacity":60});
 
-  var geocoded_clicks_path = "/home/geocoded_clicks"
+    var geocoded_clicks_path = "/home/geocoded_clicks"
 
-  if ($('#pixel_data').length > 0){
-	  geocoded_clicks_path = $('#pixel_data').attr('data-geo-path')
-  }
-  console.log(geocoded_clicks_path)
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: geocoded_clicks_path,
-    success: function(data){ 
-      mapData={
-        max: 46,
-        data: data
-      };
+    if ($('#pixel_data').length > 0){
+  	  geocoded_clicks_path = $('#pixel_data').attr('data-geo-path')
     }
-  });
-	google.maps.event.addListenerOnce(map, "idle", function(){
-    heatmap.setDataSet(mapData);
-  });
+    console.log(geocoded_clicks_path)
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: geocoded_clicks_path,
+      success: function(data){ 
+        mapData={
+          max: 46,
+          data: data
+        };
+      }
+    });
+  	google.maps.event.addListenerOnce(map, "idle", function(){
+      heatmap.setDataSet(mapData);
+    });
+
+  }
 };
